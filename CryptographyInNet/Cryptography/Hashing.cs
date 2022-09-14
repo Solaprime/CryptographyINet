7d47b19ecfc985a4d6e7c6b180652d308d2a17be
Adebanjo Oluwasola <adebanjo.oluwasola.37@gmail.com>
2022-09-14 17:18:02 +0100

    Worked with hashing using MD5 and SHA family

---
+++ CryptographyInNet/Cryptography/Hashing.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Cryptography
+{
+    public static class HashData
+    {
+        // by default we dont work with string or file, anything to br hashed is usually 
+        //converted to a byte array 
+        public static byte[] ComputeHashSha1(byte[] toBeHashed)
+        {
+            //the create  method creates an instance of the default implemantation of SHA1
+            using var sha1 = SHA1.Create();
+            return sha1.ComputeHash(toBeHashed);
+           
+        }
+
+        public static byte[] ComputeHashSha256(byte[] toBeHashed)
+        {
+            using var sha256 = SHA256.Create();
+            return sha256.ComputeHash(toBeHashed);
+        }
+
+        public static byte[] ComputeHashSha512(byte[] toBeHashed)
+        {
+            using var sha512 = SHA512.Create();
+            return sha512.ComputeHash(toBeHashed);
+        }
+
+        public static byte[] ComputeHashMd5(byte[] toBeHashed)
+        {
+            using var md5 = MD5.Create();
+            return md5.ComputeHash(toBeHashed);
+        }
+    }
+}
