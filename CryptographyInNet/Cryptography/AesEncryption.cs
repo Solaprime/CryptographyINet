83513bd58b15b88db8baca0d28da978ecc9a31c7
Adebanjo Oluwasola <adebanjo.oluwasola.37@gmail.com>
2022-09-21 17:27:23 +0100

    Worked with Aes Encryption

---
+++ CryptographyInNet/Cryptography/AesEncryption.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+//using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Cryptography
+{
+    public  class AesEncryption
+    {
+        //genrate random number
+        public byte[] GenerateRandomNumber(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+
+        // Encrypt  flow, we convert the data we want to encrypt to byte array,
+        // we conveertt the key to a byte array as well
+        // we convert the Iv intialzation vector to abyte array as well,
+        // the iv isnt a secret 
+
+        public byte[] Encrypt(byte[] dataToEncrypt, byte[] key, byte[] iv)
+        {
+            //instanciate the AesCrytoserviceprovider
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                // the mode and padding are set to their default,
+                //in most cases the deafault are okay though
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                //we pass our key and Iv 
+                aes.Key = key;
+                aes.IV = iv;
+
+                //Aes uses stream so where create a new stream
+                using (var memoryStream = new MemoryStream())
+                {
+                    // we chain the stream to a crypto stream
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(),
+                        CryptoStreamMode.Write);
+
+                    //we write our stream
+                    cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                    // we flush out the stream
+                    cryptoStream.FlushFinalBlock();
+
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+
+        // similar to the Encrypt flow
+        public byte[] Decrypt(byte[] dataToDecrypt, byte[] key, byte[] iv)
+        {
+            
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    // In abpve we called createEncryptor but here we cll createDecryptor
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(),
+                        CryptoStreamMode.Write);
+
+                    cryptoStream.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    var decryptBytes = memoryStream.ToArray();
+
+                    // return decrypt data
+                    return decryptBytes;
+                }
+            }
+        }
+    }
+}
--- CryptographyInNet/Cryptography/Program.cs
+++ CryptographyInNet/Cryptography/Program.cs
@@ -8,56 +8,66 @@ namespace Cryptography
     {
         static void Main(string[] args)
         {
-            const string originalMessage = "Original Message to hash";
-            const string originalMessage2 = "Original Message to hash 2";
-
-            Console.WriteLine("HMAC Demonstration in .NET");
-            Console.WriteLine("--------------------------");
-            Console.WriteLine();
-
-
-            // generate key that will contain our 32 bit nnumber 
-            // we called the static method in our hmac class to generate the Key
-            var key = Hmac.GenerateKey();
-
-            var hmacMd5Message = Hmac.ComputeHmacmd5(Encoding.UTF8.GetBytes(originalMessage), key);
-            var hmacMd5Message2 = Hmac.ComputeHmacmd5(Encoding.UTF8.GetBytes(originalMessage2), key);
-
-            var hmacSha1Message = Hmac.ComputeHmacsha1(Encoding.UTF8.GetBytes(originalMessage), key);
-            var hmacSha1Message2 = Hmac.ComputeHmacsha1(Encoding.UTF8.GetBytes(originalMessage2), key);
-
-            var hmacSha256Message = Hmac.ComputeHmacsha256(Encoding.UTF8.GetBytes(originalMessage), key);
-            var hmacSha256Message2 = Hmac.ComputeHmacsha256(Encoding.UTF8.GetBytes(originalMessage2), key);
-
-            var hmacSha512Message = Hmac.ComputeHmacsha512(Encoding.UTF8.GetBytes(originalMessage), key);
-            var hmacSha512Message2 = Hmac.ComputeHmacsha512(Encoding.UTF8.GetBytes(originalMessage2), key);
+          //  TestAesGCM();
 
             Console.WriteLine();
-            Console.WriteLine("MD5 HMAC");
             Console.WriteLine();
-            Console.WriteLine("Message 1 hash = " + Convert.ToBase64String(hmacMd5Message));
-            Console.WriteLine("Message 2 hash = " + Convert.ToBase64String(hmacMd5Message2));
+            Console.WriteLine();  
 
-            Console.WriteLine();
-            Console.WriteLine("SHA 1 HMAC");
-            Console.WriteLine();
-            Console.WriteLine("Message 1 hash = " + Convert.ToBase64String(hmacSha1Message));
-            Console.WriteLine("Message 2 hash = " + Convert.ToBase64String(hmacSha1Message2));
-
-            Console.WriteLine();
-            Console.WriteLine("SHA 256 HMAC");
-            Console.WriteLine();
-            Console.WriteLine("Message 1 hash = " + Convert.ToBase64String(hmacSha256Message));
-            Console.WriteLine("Message 2 hash = " + Convert.ToBase64String(hmacSha256Message2));
-
-            Console.WriteLine();
-            Console.WriteLine("SHA 512 HMAC");
-            Console.WriteLine();
-            Console.WriteLine("Message 1 hash = " + Convert.ToBase64String(hmacSha512Message));
-            Console.WriteLine("Message 2 hash = " + Convert.ToBase64String(hmacSha512Message2));
-            Console.WriteLine();
+            TestAesCBC();
 
             Console.ReadLine();
         }
+        private static void TestAesCBC()
+        {
+            const string original = "Text to encrypt";
+            var aes = new AesEncryption();
+            var key = aes.GenerateRandomNumber(32);
+            var iv = aes.GenerateRandomNumber(16);
+
+
+            var encrypted = aes.Encrypt(Encoding.UTF8.GetBytes(original), key, iv);
+            var decrypted = aes.Decrypt(encrypted, key, iv);
+
+            var decryptedMessage = Encoding.UTF8.GetString(decrypted);
+
+            Console.WriteLine("AES Encryption Demonstration in .NET");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Original Text = " + original);
+            Console.WriteLine("Encrypted Text = " + Convert.ToBase64String(encrypted));
+            Console.WriteLine("Decrypted Text = " + decryptedMessage);
+        }
+
+        //private static void TestAesGCM()
+        //{
+        //    const string original = "Text to encrypt";
+
+        //    var aesGCM = new AesGCMEncryption();
+
+        //    var gcmKey = aesGCM.GenerateRandomNumber(32);
+        //    var nonce = aesGCM.GenerateRandomNumber(12);
+
+        //    try
+        //    {
+        //        (byte[] ciphereText, byte[] tag) result = aesGCM.Encrypt(Encoding.UTF8.GetBytes(original), gcmKey, nonce, Encoding.UTF8.GetBytes("some metadata"));
+        //        byte[] decryptedText = aesGCM.Decrypt(result.ciphereText, gcmKey, nonce, result.tag, Encoding.UTF8.GetBytes("some metadata"));
+
+        //        Console.WriteLine("AES GCM Encryption Demonstration in .NET");
+        //        Console.WriteLine("----------------------------------------");
+        //        Console.WriteLine();
+        //        Console.WriteLine("Original Text = " + original);
+        //        Console.WriteLine("Encrypted Text = " + Convert.ToBase64String(result.ciphereText));
+        //        Console.WriteLine("Decrypted Text = " + Encoding.UTF8.GetString(decryptedText));
+        //    }
+        //    catch (CryptographicException ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //    catch (ArgumentException ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //} 
     }
 }
